17241c94139df5eccb1c3bdf00865d6e4bdb27c2
rahat-495 <147464855+rahat-495@users.noreply.github.com>
2024-06-09 15:13:07 +0600

    main work are done

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,8 +1,58 @@
 
+import { IoCall } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { Input, Textarea } from "@material-tailwind/react";
+
 const ContactUs = () => {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center gap-5 w-full min-h-[70vh]">
             
+            <h1 className="gro my-10 text-4xl font-semibold">Contact Us</h1>
+
+            <div className="flex flex-col items-start justify-between gro gap-10">
+                    
+                <div className="flex items-center justify-between gap-10">
+
+                    <div className="flex items-center flex-col justify-center">
+                        <p className="p-3 bg-[#E91E63] rounded-full text-center text-white">
+                            <IoCall className="text-3xl"/>
+                        </p>
+                        <p className="my-2 text-xl">Call us from 10 am to 7 pm</p>
+                        <p className="">+61 383 765 284</p>
+                    </div>
+
+                    <div className="flex items-center flex-col justify-center">
+                        <p className="p-3 bg-[#E91E63] rounded-full text-center text-white">
+                            <FaLocationDot className="text-3xl"/>
+                        </p>
+                        <p className="my-2 text-xl">Our location</p>
+                        <p className="">245 Quigley Blvd, Ste K</p>
+                    </div>
+
+                    <div className="flex items-center flex-col justify-center">
+                        <p className="p-3 bg-[#E91E63] rounded-full text-center text-white">
+                            <MdEmail className="text-3xl"/>
+                        </p>
+                        <p className="my-2 text-xl">Our Team Email</p>
+                        <p className="">+61 383 765 284</p>
+                    </div>
+
+                </div>
+
+                <div className="w-full">
+                    <form className="flex flex-col gap-5">
+                        <div className="flex flex-col items-center gap-5">
+                            <Input className="" type="text" label="Name" required/>
+                            <Input className="" type="email" label="Email" required/>
+                        </div>
+                        <Textarea required label="Enter Your Message"/>
+                        <input type="submit" value={'Send'} className="btn btn-outline hover:bg-[#E91E63]"/>
+                    </form>
+                </div>
+
+            </div>
+
         </div>
     );
 };
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -3,6 +3,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
+import { Button } from '@material-tailwind/react';
 
 const Banner = () => {
     return (
@@ -19,36 +20,39 @@ const Banner = () => {
                     <div className="hero min-h-[70vh] rounded-b-lg" style={{backgroundImage: 'url(https://assets-global.website-files.com/627a7356ad04a22fd1133827/650849544a05f67c8cac86ec_Panenco%20at%20Healthcare%20Excellence%20Through%20Technology.jpg)'}}>
                         <div className="hero-overlay bg-opacity-30 rounded-b-lg"></div>
                         <div className="hero-content text-center text-neutral-content">
-                            <div className="max-w-md">
-                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                            <div className="max-w-lg">
+                                
+                                <h1 className="mb-5 text-5xl font-bold">Celebrating 10 Years of Excellence</h1>
+                                <p className="mb-5">A decade of innovation, growth, and success. Thank you for being part of our journey!</p>
+                                
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <div className="hero min-h-[70vh] rounded-b-lg" style={{backgroundImage: 'url(https://assets-global.website-files.com/627a7356ad04a22fd1133827/650849544a05f67c8cac86ec_Panenco%20at%20Healthcare%20Excellence%20Through%20Technology.jpg)'}}>
+                    <div className="hero min-h-[70vh] rounded-b-lg" style={{backgroundImage: 'url(https://img.freepik.com/premium-photo/young-man-white-shirt-giving-thumbs-up-sign_889056-253853.jpg?size=626&ext=jpg)'}}>
                         <div className="hero-overlay bg-opacity-30 rounded-b-lg"></div>
                         <div className="hero-content text-center text-neutral-content">
-                            <div className="max-w-md">
-                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                            <div className="max-w-lg">
+                                
+                                <h1 className="mb-5 text-5xl font-bold">Over 1 Million Happy Customers</h1>
+                                <p className="mb-5">We are proud to have served over a million satisfied customers around the globe.</p>
+                                
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <div className="hero min-h-[70vh] rounded-b-lg" style={{backgroundImage: 'url(https://assets-global.website-files.com/627a7356ad04a22fd1133827/650849544a05f67c8cac86ec_Panenco%20at%20Healthcare%20Excellence%20Through%20Technology.jpg)'}}>
+                    <div className="hero min-h-[70vh] rounded-b-lg" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1579548122080-c35fd6820ecb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJhbm5lcnxlbnwwfHwwfHx8MA%3D%3D)'}}>
                         <div className="hero-overlay bg-opacity-30 rounded-b-lg"></div>
                         <div className="hero-content text-center text-neutral-content">
-                            <div className="max-w-md">
-                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                            <div className="max-w-lg">
+                                
+                                <h1 className="mb-5 text-5xl font-bold">Award-Winning Solutions</h1>
+                                <p className="mb-5">Recognized for our outstanding contributions to industry innovation and customer satisfaction.</p>
+                                
                             </div>
                         </div>
                     </div>
